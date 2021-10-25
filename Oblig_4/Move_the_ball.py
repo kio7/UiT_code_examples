fbd709af0436b72679f626bc1deaba3bda513e14
Konrad Simsø <konrad.simso@gmail.com>
2021-10-25 13:46:50 +0200

    Update Move_the_ball.py

--- Oblig_4/Move_the_ball.py
+++ Oblig_4/Move_the_ball.py
@@ -3,7 +3,7 @@ from tkinter import *
 class Panel:
     def __init__(self):
         window = Tk() 
-        window.title("Move a Ball") 
+        window.title("Moving ball") 
         
         self.canvas = Canvas(window, width = 500, height = 250, 
             bg = "light gray")
