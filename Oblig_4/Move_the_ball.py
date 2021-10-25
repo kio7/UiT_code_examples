2e4cb4df38d8e4d69eb28d313308a05bbfbd4ad1
Konrad Simsø <konrad.simso@gmail.com>
2021-10-25 13:48:47 +0200

    Update Move_the_ball.py

--- Oblig_4/Move_the_ball.py
+++ Oblig_4/Move_the_ball.py
@@ -12,24 +12,20 @@ class Panel:
         frame = Frame(window)
         frame.pack()
         
-        bt_circle = Button(frame, text = "Circle", command = self.display_circle)
         bt_left = Button(frame, text = "Left", command = self.left)
         bt_right = Button(frame, text = "Right", command = self.right)
         bt_up = Button(frame, text = "Up", command = self.up)
         bt_down= Button(frame, text = "Down", command = self.down)
 
-        bt_circle.grid(row = 1, column = 2)
-        bt_left.grid(row = 2, column = 1)
-        bt_right.grid(row = 2, column = 2)
-        bt_up.grid(row = 2, column = 3)
-        bt_down.grid(row = 2, column = 4)
-
+        bt_left.grid(row = 1, column = 1)
+        bt_right.grid(row = 1, column = 2)
+        bt_up.grid(row = 1, column = 3)
+        bt_down.grid(row = 1, column = 4)
         
+        self.canvas.create_oval(130, 130, 150, 150, fill = "red", tags = "Circle", )
+
         window.mainloop()
        
-    def display_circle(self):
-        self.canvas.create_oval(130, 130, 150, 150, fill = "red", tags = "Circle", )
-    
     def right(self):
         position = self.canvas.coords("Circle")
         if position[2] < 500:
@@ -50,4 +46,4 @@ class Panel:
         if position[3] < 250:
             self.canvas.move("Circle", 0, +10)
 
-Panel() 
+Panel()
