69b7e07d3eb4165e2bed3b434ec48ae0ba533878
kio7 <konrad.simso@gmail.com>
2021-10-26 14:32:28 +0200

    Update Move_the_ball.py

--- Oblig_4/Move_the_ball.py
+++ Oblig_4/Move_the_ball.py
@@ -1,28 +1,28 @@
-from tkinter import * 
+import tkinter as tk
     
 class Panel:
     def __init__(self):
-        window = Tk() 
+        window = tk.Tk() 
         window.title("Moving Ball") 
         
-        self.canvas = Canvas(window, width = 500, height = 250, bg = "light gray")
+        self.canvas = tk.Canvas(window, width = 500, height = 250, bg = "light gray")
         self.canvas.pack()
         
-        frame = Frame(window)
+        frame = tk.Frame(window)
         frame.pack()
         
-        bt_left = Button(frame, text = "Left", command = self.left)
-        bt_right = Button(frame, text = "Right", command = self.right)
-        bt_up = Button(frame, text = "Up", command = self.up)
-        bt_down= Button(frame, text = "Down", command = self.down)
+        bt_left = tk.Button(frame, text = "Left", command = self.left)
+        bt_right = tk.Button(frame, text = "Right", command = self.right)
+        bt_up = tk.Button(frame, text = "Up", command = self.up)
+        bt_down= tk.Button(frame, text = "Down", command = self.down)
 
         bt_left.grid(row = 1, column = 1)
         bt_right.grid(row = 1, column = 2)
         bt_up.grid(row = 1, column = 3)
         bt_down.grid(row = 1, column = 4)
         
-        self.canvas.create_oval(130, 130, 150, 150, fill = "red", tags = "Circle", )
-
+        self.canvas.create_oval(240, 115, 270, 145, fill = "red", tags = "Circle", )
+        #self.canvas.create_oval()
         window.mainloop()
        
     def right(self):
@@ -37,12 +37,12 @@ class Panel:
     
     def up(self):
         position = self.canvas.coords("Circle")
-        if position[1] > 0:
+        if position[1] > 10:
             self.canvas.move("Circle", 0, -10)
 
     def down(self):
         position = self.canvas.coords("Circle")
-        if position[3] < 250:
+        if position[3] < 240:
             self.canvas.move("Circle", 0, +10)
 
 Panel()
