8e7a3170e9c9392dea0dca66c88223158d120dce
kio7 <konrad.simso@gmail.com>
2021-11-05 11:37:15 +0100

    Filling in ATM_machine
    Adding all needed features to ATM_machine

--- Oblig_5/ATM_machine.py
+++ Oblig_5/ATM_machine.py
@@ -1,4 +1,4 @@
-from Account_class import Account as Ac
+from Account_class import Account
 
 def main_menu():
     while True:
@@ -15,13 +15,44 @@ def main_menu():
             break
 
     return choice
-def something():
-    pass
+
+def choice_handler(accounts, choice, id):
+    if choice == 1:
+        print(f"The balance is {accounts[id].balance}")
+
+    if choice == 2:
+        amount_to_withdraw = int(input("Enter an amount to withdraw: "))
+        accounts[id].withdraw(amount_to_withdraw)
+
+    if choice == 3:
+        amount_to_deposit = int(input("Enter an amount to deposit: "))
+        accounts[id].deposit(amount_to_deposit)
+
+
+def main_system(accounts):   
+    while True:
+        user_id = int(input("Enter an id please: "))
+        
+        if user_id < 0 or user_id > 9:
+            print("Innvalid id, please try again.")
+        else:
+            break
+
+    while True:
+        choice = main_menu()
+        if choice == 4:
+            break
+        choice_handler(accounts, choice, user_id)
+
 
 def main():
+    accounts = []
+    for x in range(10):
+        accounts.append(Account(x, 100, 0))
     
+    while True:
+        main_system(accounts)
     
-    pass
 
 if __name__ == "__main__":
     main()
