c21a52ec834e70d6967bfb2961134e488662e025
Konrad Simsø <konrad.simso@gmail.com>
2021-11-08 17:34:32 +0100

    redid Anagram-2
    had to redo it since it was not working properly

--- Oblig_3/Anagram-2.py
+++ Oblig_3/Anagram-2.py
@@ -1,9 +1,22 @@
-def is_anagram(lis1, lis2):
-    if list(lis1).sort() == list(lis2).sort(): return True
+def is_anagram(str1, str2):
+    lis1, lis2 = [], []
+
+    for elem in str1:
+        lis1.append(elem)
+    for elem in str2:
+        lis2.append(elem)
+        
+    lis1 = sorted(lis1)
+    lis2 = sorted(lis2)
+
+    if lis1 == lis2:
+        return True
+    else:
+        return False
 
 def main():
-    lis1, lis2 = input("Enter a string: ").lower(), input("Enter another string: ").lower()
-    print(f"Is{'' if is_anagram(lis1, lis2) else ' not'} Anagram.")
+    str1, str2 = input("Enter a string: ").lower(), input("Enter another string: ").lower()
+    print(f"Is{'' if is_anagram(str1, str2) else ' not'} Anagram.")
     
 if __name__ == "__main__":
     main()
