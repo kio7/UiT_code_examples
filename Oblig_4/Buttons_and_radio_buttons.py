fe6ddb82811ce77fb5c6929cf5dceb493ca1cccc
kio7 <konrad.simso@gmail.com>
2021-11-15 10:15:51 +0100

    Update Buttons_and_radio_buttons.py

--- Oblig_4/Buttons_and_radio_buttons.py
+++ Oblig_4/Buttons_and_radio_buttons.py
@@ -9,14 +9,21 @@ class Panel:
         frame = tk.Frame(window, width = 500, height = 250, bg = "gray")
         frame.pack()
         
-        bt_something = tk.Button(frame, text = "Left", command = self.handler_something)
-       
+        text_canvas = tk.Canvas()
 
-        bt_something.grid(row = 1, column = 1)
-        
+
+        bt_left = tk.Button(frame, text = "Left", command = self.handler_left)
+        bt_right = tk.Button(frame, text = "Right", command = self.handler_right)
+
+        bt_left.grid(row = 1, column = 1)
+        bt_right.grid(row = 1, column = 2)
+
         window.mainloop()
        
-    def handler_something(self):
+    def handler_left(self):
+        pass
+
+    def handler_right(self):
         pass
 
 Panel()
