22d3ff39cde469616396d256a25b4b965bd376ac
Konrad Simsø <konrad.simso@gmail.com>
2021-11-16 19:50:19 +0100

    Clock and Test_Clock
    Doing Unittesting and finding a lot of errors/fails.

--- Oblig_5/Clock.py
+++ Oblig_5/Clock.py
@@ -9,50 +9,51 @@ class Clock:
     
     def inc_second(self):
         if self.sec >= 59:
-            self.sec = 0
             self.inc_minute()
+            self.sec = 0
         else:
             self.sec += 1
         
     def inc_minute(self):
         if self.min >= 59:
-            self.min = 0
             self.inc_hour()
+            self.min = 0
         else:
             self.min += 1
 
     def inc_hour(self):
         if self.hour >= 23:
-            self.hour = 0
             self.inc_day()
+            self.hour = 0
         else:
             self.hour += 1
 
     def inc_day(self):
         if self.month == 1 or self.month == 3 or self.month == 5 or self.month == 7 or self.month == 8 or self.month == 10 or self.month == 12:
             if self.day >= 31:
-                self.day = 0
                 self.inc_month()
+                self.day = 1
             else:
                 self.day += 1
-        if self.month == 2:
+        elif self.month == 2:
             if (self.year % 4 == 0 and self.year % 100 != 0) or (self.year % 400 == 0):
                 if self.day >= 29:
-                    self.day = 0
                     self.inc_month()
+                    self.day = 1
                 else:
                     self.day += 1
             else:
                 if self.day >= 28:
-                    self.day = 0
                     self.inc_month()
+                    self.day = 1
                 else:
                     self.day += 1
-
+        else:
+            self.day += 1
     def inc_month(self):
         if self.month >= 12:
-            self.month = 0
             self.inc_year()
+            self.month = 1
         else:
             self.month += 1
 
@@ -60,7 +61,7 @@ class Clock:
         self.year += 1
 
     def clock_as_string(self):
-        string = f"{self.day:02d}:{self.month:02d}:{self.year}:{self.hour:02d}:{self.min:02d}:{self.sec:02d}"
+        string = f"{self.day:02d}:{self.month:02d}:{self.year:04d}:{self.hour:02d}:{self.min:02d}:{self.sec:02d}"
 
         return string
 
@@ -71,8 +72,3 @@ class Clock:
         self.hour = hour
         self.min = min
         self.sec = sec
-
-if __name__ == "__main__":
-    clock = Clock(1,12,2021,2,9,3)
-
-    print(clock.clock_as_string())
--- Oblig_5/Test_Clock.py
+++ Oblig_5/Test_Clock.py
@@ -3,19 +3,75 @@ import unittest
 
 class Test_Clock(unittest.TestCase):
     
-    def set_up(self):
+    def setUp(self):
         self.__clock = Clock()
     
+    # From Default values
     def test_inc_sec_from_default_values(self):
-        self.__clock.inc_sec()
+        self.__clock.inc_second()
         self.assertEqual(self.__clock.clock_as_string(), "00:00:0000:00:00:01")
     
     def test_inc_min_from_default_values(self):
-        self.__clock.inc_min()
+        self.__clock.inc_minute()
         self.assertEqual(self.__clock.clock_as_string(), "00:00:0000:00:01:00")
 
+    def test_inc_hour_from_default_values(self):
+        self.__clock.inc_hour()
+        self.assertEqual(self.__clock.clock_as_string(), "00:00:0000:01:00:00")
+    
+    def test_inc_day_from_default_values(self):
+        self.__clock.inc_day()
+        self.assertEqual(self.__clock.clock_as_string(), "01:00:0000:00:00:00")
+    
+    def test_inc_month_from_default_values(self):
+        self.__clock.inc_month()
+        self.assertEqual(self.__clock.clock_as_string(), "00:01:0000:00:00:00")
+    
+    def test_inc_year_from_default_values(self):
+        self.__clock.inc_year()
+        self.assertEqual(self.__clock.clock_as_string(), "00:00:0001:00:00:00")
+    
+    # Border and "sunshine" scenarios
     def test_inc_sec_from_midnight_jan(self):
-        self.__clock = Clock(30, 1, 2021, 23, 59, 59)
-        self.__clock.inc_sec()
+        self.__clock = Clock(31, 1, 2021, 23, 59, 59)
+        self.__clock.inc_second()
         self.assertEqual(self.__clock.clock_as_string(), "01:02:2021:00:00:00")
-        
+    
+    def test_inc_min_from_midnight_jan(self):
+        self.__clock = Clock(31, 1, 2021, 23, 59, 59)
+        self.__clock.inc_minute()
+        self.assertEqual(self.__clock.clock_as_string(), "01:02:2021:00:00:59")
+    
+    def test_inc_hour_from_midnight_jan(self):
+        self.__clock = Clock(31, 1, 2021, 23, 59, 59)
+        self.__clock.inc_hour()
+        self.assertEqual(self.__clock.clock_as_string(), "01:02:2021:00:59:59")
+    
+    def test_inc_day_from_new_years(self):
+        self.__clock = Clock(31, 12, 2021, 23, 59, 59)
+        self.__clock.inc_day()
+        self.assertEqual(self.__clock.clock_as_string(), "01:01:2022:23:59:59")
+    
+    def test_inc_month_from_new_years(self):
+        self.__clock = Clock(31, 12, 2021, 23, 59, 59)
+        self.__clock.inc_month()
+        self.assertEqual(self.__clock.clock_as_string(), "31:01:2022:23:59:59")
+    
+    def test_inc_year_for_fun(self):
+        self.__clock = Clock(5, 12, 2500, 12, 45, 00)
+        self.__clock.inc_year()
+        self.assertEqual(self.__clock.clock_as_string(), "05:12:2501:12:45:00")
+    
+    # Leap Year Tests
+    def test_leap_year(self):
+        self.__clock = Clock(28, 2, 2000, 23, 59, 59)
+        self.__clock.inc_second()
+        self.assertEqual(self.__clock.clock_as_string(), "29:02:2000:00:00:00")
+
+    def test_not_leap_year(self):
+        self.__clock = Clock(28, 2, 2001, 23, 59, 59)
+        self.__clock.inc_second()
+        self.assertEqual(self.__clock.clock_as_string(), "01:03:2001:00:00:00")
+    
+if __name__ == "__main__":
+    unittest.main()
