13e6d0705e1659a05664eaf045bc3dbff79e73d5
Konrad Simsø <konrad.simso@gmail.com>
2022-01-17 10:55:18 +0100

    Adding some folders and files

---
+++ VP/undervisning/undervis_14_01.py
@@ -0,0 +1,51 @@
+# Oppg. 5.26
+
+# teller = 1
+# nevner = 3
+# temp = 0
+
+# while teller <= 97:
+#     temp += teller / nevner
+#     teller += 2
+#     nevner += 2
+
+# print(temp)
+
+# def sum_series():
+#    sum = 0
+#    n = 1
+#    while n <= 49:
+#        q = (2*n - 1)
+#        p = (2*n + 1)
+#        sum += (q / p)
+#        n += 1
+#    return sum
+       
+# print(sum_series())
+
+# def compute_pi():
+#     pi = 4
+#     i = 1
+#     total = 0
+#     while i <= 10000000:
+#         formel = ((-1)**(i + 1))/(2*i - 1)
+#         total += formel
+#         i +=1
+#     print(f"{pi * total:.90f}")
+
+# compute_pi()
+
+
+# from math import factorial
+
+# def compute_e():
+#     i = 1
+#     result = 1
+    
+#     while i <= 10000:
+#         result += 1 / factorial(i)
+#         i += 1
+#     print(f"{result:.30f}")
+
+# compute_e()
+
---
+++ VP/undervisning/undervisning.py
@@ -0,0 +1,25 @@
+# Primitives
+from cgi import test
+
+
+my_string: str = "whatever"
+my_int: int = 100
+my_float: float = 12.3999
+my_large_float: float = 12.312e10
+my_bool: bool = False
+
+# Sets
+my_set: set =  {1, "Flask", 1.23, "Flask"}
+
+# Sequenses
+my_range: range = range(11, 0, -2)
+my_list: list = [1, 2, 3]
+my_tuple: tuple = (1, [1,2,3], (1,2), "Alo") #Can't be changed
+
+# Dictionaries
+my_dictionary: dict = {1 : "one", 2 : "two", 3 : "three"}
+
+# Changing
+my_list.append("Whatever")
+my_list.insert(2, "Changed")
+
