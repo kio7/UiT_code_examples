9aa41cd6d22decdb23d2782d28f61f270fd92a6e
kio7 <konrad.simso@gmail.com>
2022-02-11 10:40:01 +0100

    Update Anagrams.py

--- VP/Oblig_2/Anagrams.py
+++ VP/Oblig_2/Anagrams.py
@@ -1,2 +1,18 @@
-str1 = input("Enter a string: ").lower()
-str2 = input("Enter another string: ").lower()
+def test_anagram(dict):
+    string_one = ''.join(sorted(dict.get("one")))
+    string_two = ''.join(sorted(dict.get("two")))
+    
+    if string_one == string_two: return True
+
+
+def main():
+    str1 = input("Enter a string: ").lower()
+    str2 = input("Enter another string: ").lower()
+
+    dict_of_strings = {"one": str1, "two": str2}
+
+    print(f"The 2 strings {'are' if test_anagram(dict_of_strings) else 'are not'} Anagram.")
+
+
+if __name__ == "__main__":
+    main()
