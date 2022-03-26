c01829be546a107a5b5fddbd74fcadd6f3c1b849
kio7 <konrad.simso@gmail.com>
2022-03-26 15:03:45 +0100

    adding all oblig_4

--- VP/Oblig_4/postorder_stack.py
+++ VP/Oblig_4/postorder_stack.py
@@ -16,30 +16,6 @@ class Stack:
         else:
             return self.__elements[-1]
 
-<<<<<<< Updated upstream
-    def left_child(self, index):
-        if len(self.__lis) > (2 * index) + 1:
-            if self.__lis[(2 * index) + 1] == None:
-                return None
-            return self.__lis[(2 * index) + 1]
-
-    def right_child(self, index):
-        if len(self.__lis) > (2 * index) + 2:
-            if self.__lis[(2 * index) + 2] == None:
-                return None
-            return self.__lis[(2 * index) + 2]
-    
-    def index(self, elm):
-        return self.__lis.index(elm)
-    
-    def remove(self, elm):
-        self.__lis.pop(self.__lis.index(elm))
-    
-    def replace_with_None(self, elm):
-        self.__lis[self.__lis.index(elm)] = None
-        
-
-=======
     # Stores an element into the top of the stack
     def push(self, value):    
         self.__elements.append(value)
@@ -52,62 +28,10 @@ class Stack:
             return self.__elements.pop() 
     
     # Return the size of the stack
->>>>>>> Stashed changes
     def __len__(self):
         return len(self.__elements)
 
 
-<<<<<<< Updated upstream
-def post_order(elem):
-    original = elem.copy()
-    stack_2 = Stack()
-    stack_2.push(elem[0])
-
-    stack_1 = Stack()
-    stack_1.push(elem)
-
-    current = None
-    tag = 0
-
-
-    while len(original) > len(stack_2):
-        if current != stack_2.peek(): #
-            previous = current
-            current = stack_2.peek()
-        else:
-            current = previous
-        if tag:
-            current = elem[int((index / 2) - 1)]
-            tag = 0
-        
-        if stack_1.right_child(elem.index(current)):
-            stack_2.push(elem[(2 * elem.index(current)) + 2])
-
-        elif stack_1.left_child(elem.index(current)):
-            stack_2.push(elem[(2 * elem.index(current)) + 1])
-
-        else:
-            to_be_removed = stack_2.peek()
-            if to_be_removed in elem:
-                stack_1.remove(to_be_removed)
-                del elem[elem.index(to_be_removed)]
-            else:
-                index = elem.index(current)
-                stack_1.replace_with_None(current)
-                tag = 1
-
-    for _ in range(len(stack_2)):
-        print(stack_2.pop())
-
-
-if __name__ == '__main__':
-    elements = ["Emma", "Isak", "Anders", 'Helge', '1', '2', 10]
-    post_order(elements)
-
-    # Post order should be: Helge, 1, Isak, 2, 10, Anders, Emma
-
-    # Stack Oder: Emma, Anders, 10, 2, Isak, 1, Helge
-=======
 class Node:
     def __init__(self, e):
         self.element = e
@@ -175,4 +99,3 @@ if __name__ == '__main__':
  
     postorderIterative(root)
     # Print of this tree should be 4, 2, 7, 8, 5, 6, 3, 1
->>>>>>> Stashed changes
